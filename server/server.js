fa2a044fd367c49ae25a9b4bb7787d8ab3d52e03
Marta Jopp <martapeterson@gmail.com>
2018-03-22 07:30:16 -0500

    router

--- server/server.js
+++ server/server.js
@@ -3,9 +3,15 @@ var app = express();
 var port = process.env.PORT || 5000; // listen on port 5000
 var bodyParser = require('body-parser'); 
 
+var registerRouter = require('./routes/register.router');
+
+
 app.use(bodyParser.json());
 // static files
 app.use(express.static('server/public'));
+
+app.use('/register', registerRouter);
+
 
 app.listen(port, function () {
     console.log('Listening on port', port) // lets us know server is working
