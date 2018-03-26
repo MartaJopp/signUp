64edd37e2febb74a9de1d576fa95e1db2f7e9e7a
Marta Jopp <martapeterson@gmail.com>
2018-03-25 21:06:14 -0500

    post route

--- server/public/scripts/services/class.service.js
+++ server/public/scripts/services/class.service.js
@@ -2,9 +2,15 @@ myApp.service('ClassService', function ($http, $location) {
     console.log('ClassService Loaded');
     var self = this;
 
-self.makeNewClass = function (newClass) {
-    console.log('the class', newClass)
-}
+    self.makeNewClass = function (newClass) {
+        console.log('the class', newClass)
+
+        $http.post('/admin/', newClass).then(function (response) {
+            console.log(response)
+        }).catch(function (err) {
+            console.log('Error Posting Class');
+        })
+    }
 
 
 
