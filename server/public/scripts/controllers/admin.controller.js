e267134659a3f37a656fbaadb14b577ddab4cf34
Marta Jopp <martapeterson@gmail.com>
2018-03-26 21:01:28 -0500

    new instructor

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -1,14 +1,19 @@
 myApp.controller('AdminController', function (HomeService, ClassService) {
     var vm = this;
     vm.classService = ClassService;
-    vm.listOfClasses = ['beginner', 'advanced', 'intermediate']
-
+    vm.listOfClasses = [{class: 'beginner',
+    id: 1},
+    {class: 'advanced', id: 2}, {class: 'intermediate', id: 3}]
+    vm.classes = []
     //sets up a new class
     vm.makeNewClass = function (newClass) {
         console.log('clicked')
         ClassService.makeNewClass(newClass)
     } //end make new class
 
-
+vm.addInstructor = function (instructor, classes) {
+    // console.log('add instructor clicked', classes)
+    ClassService.addInstructor(instructor, classes)
+}
 
 }) // end Admin controller
--- server/public/scripts/services/class.service.js
+++ server/public/scripts/services/class.service.js
@@ -2,6 +2,13 @@ myApp.service('ClassService', function ($http, $location) {
     console.log('ClassService Loaded');
     var self = this;
 
+    self.newInstructor = {
+        firstName: '',
+        lastName: '',
+        goesBy: '',
+        classesTaught: []
+    }
+
     self.makeNewClass = function (newClass) {
         console.log('the class', newClass)
 
@@ -12,7 +19,14 @@ myApp.service('ClassService', function ($http, $location) {
         })
     }
 
-
+//add new instructor
+self.addInstructor = function (instructor, classes) {
+    
+    // console.log('instructor', classes)
+    // classes = self.newInstructor.classesTaught
+    // self.newInstructor.classesTaught = classes;
+    // console.log('self.newInstructor.classesTaught', self.newInstructor.classesTaught)
+} //end add new instructor
 
 
 }) //end service
