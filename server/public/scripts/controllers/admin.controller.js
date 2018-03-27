52c2a003b466f5cb29dcfeaafc980953f88a740e
Marta Jopp <martapeterson@gmail.com>
2018-03-26 20:40:09 -0500

    checkboxes

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -1,8 +1,8 @@
 myApp.controller('AdminController', function (HomeService, ClassService) {
     var vm = this;
     vm.classService = ClassService;
+    vm.listOfClasses = ['beginner', 'advanced', 'intermediate']
 
-    
     //sets up a new class
     vm.makeNewClass = function (newClass) {
         console.log('clicked')
